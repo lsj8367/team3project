328e6904e326b03822495abb6323c6360cadea26
HNew <46247793+HNew@users.noreply.github.com>
2020-12-22 14:47:36 +0900

    views.py

--- travel_recommend/travel/views.py
+++ travel_recommend/travel/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
 from django.http.response import HttpResponseRedirect
-from pymongo import MongoClient
-
-'''
-client = MongoClient('mongodb://localhost:27017/')
-print(client.list_database_names()) # 데이터 베이스 목록 조회
-
-db = client['데이터베이스 이름']
-conn = db['컬렉션이름']
-'''
+from django.urls import reverse
 
 # Create your views here.
 def MainFunc(request):
@@ -19,23 +11,54 @@ def SearchFunction(request):
         search = request.POST.get('search')
         start_date = request.POST.get('start_date')
         end_date = request.POST.get('end_date')
+        weather = 'rainy'
+
         if start_date == '':
             start_date = None
+            weather = ''
         if end_date == '':
             end_date = None
+            weather = ''    
         
         print(search)
         print(start_date)
         print(end_date)
         
-        weather = 'rainy'
+        
+        
         root = ['루트1', '루트2', '루트3', '루트4', '루트5']
         tour = ['여행지1', '여행지2', '여행지3', '여행지4', '여행지5']
         restaurant = ['음식점1', '음식점2', '음식점3', '음식점4', '음식점5']
         
         
-        context={'travel':search, 'start':start_date, 'end':end_date, 'root':root, 'tour':tour, 'restaurant':restaurant}
+        context={'travel':search, 'start':start_date, 'end':end_date, 'weather':weather, 'root':root, 'tour':tour, 'restaurant':restaurant}
+        #return render(request, 'main.html', context)
         return render(request, 'main.html', context)
+        return HttpResponseRedirect(reverse(''))
+    
     
 def DetailFunction(request):
-    return render(request, 'datail.html')
+    
+    return render(request, 'detail2.html')
+
+def SignupFunction(request):
+    
+    return render(request, 'signup.html')
+
+def SignupFunction2(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        ID = request.POST.get('ID')
+        password = request.POST.get('password')
+        travel1 = request.POST.get('travel1')
+        rating1 = request.POST.get('rating1')
+        travel2 = request.POST.get('travel2')
+        rating2 = request.POST.get('rating2')
+        travel3 = request.POST.get('travel3')
+        rating3 = request.POST.get('rating3')
+        
+        print(name, ID, password)
+        print(travel1, travel2, travel3)
+        print(rating1, rating2, rating3)
+        
+        return render(request, 'main.html')
